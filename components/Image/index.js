bb8e8247866d147dee4ac6f3e1a8ca81d57af738
aslihanozfidan <aslihanozfidan@gmail.com>
2019-02-20 22:17:34 +0300

    add image component

---
+++ components/Image/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.scss";
+
+export default () => (
+	<React.Fragment>
+		<img src={require("../../static/images/0.jpg")} alt="" />
+		<img src={require("../../static/images/1.jpg")} alt="" />
+		<img src={require("../../static/images/2.jpg")} alt="" />
+	</React.Fragment>
+);
